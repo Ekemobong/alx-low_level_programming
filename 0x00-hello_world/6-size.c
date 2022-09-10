8ee2add11151fe888145e4ac57ef0d7864c4cf6a
Stapha <ekemobong@yahoo.com>
2022-09-09 19:19:32 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 /**
  * main - main block
  * Return: 0
- */
-int main(void)
+ * /
+ int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+clrscr();
+printf(“TypettttSize (bytes)”);
+printf(“nCharacterttt    %d”,sizeof(char));
+printf(“nIntegertttt    %d”,sizeof(int));
+printf(“nLong intttt    %d”,sizeof(long int));
+printf(“nFloattttt    %d”,sizeof(float));
+printf(“nDoubletttt    %d”,sizeof(double));
+printf(“nLong doublettt    %d”,sizeof(long double));
+getch();
 }
